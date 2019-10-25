4f41af0f7bbe457ab6a4acb2aebdd9273026dbc5
Hans Foster <fosterhans@gmail.com>
2019-10-25 17:54:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,10 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    line_string = "The line is currently: "
+    line_string = "The line is currently:"
     counter = 1
     katz_deli.each do |line_spot|
-      line_string += "#{counter}. #{line_spot} "
+      line_string += " #{counter}. #{line_spot}"
       counter += 1 
     end
     puts line_string
