648e7429ec309560f45b21c054014f0537bf8027
Hans Foster <fosterhans@gmail.com>
2019-10-25 17:38:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,15 @@
 # Write your code here.
 
 def line(katz_deli)
-  puts ""
+  if katz_deli.size == 0
+    print "The line is currently empty."
+  else
+    print "The line is currently: "
+  counter = 1
+  katz_deli.each do |line_spot|
+    print "#{counter}. #{line_spot} "
+    counter += 1 
+  end
 end
 
 def take_a_number
